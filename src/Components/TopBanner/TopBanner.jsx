20b08e9a7d7c874073f19808b56bc1770d7b308c
Muhammad Junaid Rehman <jbhai8058@gmail.com>
2023-05-11 15:32:21 +0500

    make title dynamically

--- src/Components/TopBanner/TopBanner.jsx
+++ src/Components/TopBanner/TopBanner.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import { Fragment } from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import RestClient from '../../Rest Api/RestClient';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import AppUrl from '../../Rest Api/AppUrl';
 
 
 const TopBanner = () => {
+
+  const [title, setTitle] = useState('...');
+  const [subtitle, setSubtitle] = useState('...');
+
+  useEffect(() => {
+    RestClient.GetRequest(AppUrl.HomeTopTitle).then((result) => {
+      setTitle(result[0]['home_title']);
+      setSubtitle(result[0]['home_subtitle']);
+    }).catch(error => {
+      console.log(error);
+    });
+  },[]);
+
   return (
     <Fragment>
       <Container className='topfixedbanner p-0' fluid>
@@ -11,8 +28,8 @@ const TopBanner = () => {
           <Container className='topContent'>
             <Row>
               <Col className='text-center'>
-                <h1 className='topTitle'>EASY STANDING</h1>
-                <h4 className='topSubTitle'>LEARN PROFESSIONALY</h4>
+                <h1 className='topTitle'>{title}</h1>
+                <h4 className='topSubTitle'>{subtitle}</h4>
                 <Button variant='success'>Learn More</Button>
               </Col>
             </Row>
