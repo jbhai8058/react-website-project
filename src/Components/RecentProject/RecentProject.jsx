ae538398cc5811400c8b661ad0e7f2b36ed634fb
Muhammad Junaid Rehman <jbhai8058@gmail.com>
2023-05-11 22:36:27 +0500

    make Project  page dynamically

--- src/Components/RecentProject/RecentProject.jsx
+++ src/Components/RecentProject/RecentProject.jsx
@@ -1,55 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Fragment } from 'react'
 import { Button, Card, Col, Container, Row } from 'react-bootstrap'
-import image3 from '../../assets/images/project3.jpg';
 import { Link } from 'react-router-dom';
+import RestClient from '../../Rest Api/RestClient';
+import AppUrl from '../../Rest Api/AppUrl';
 
 const RecentProject = () => {
+
+
+    const [Data, setData] = useState([]);
+
+    useEffect(() => {
+        RestClient.GetRequest(AppUrl.ProjectHome).then((result) => {
+            setData(result);
+        }).catch(error => {
+            console.log(error);
+        });
+    }, []);
+
+    const MyView = Data.map(myItem => (
+        <Col lg={4} md={6} sm={12}  key={myItem.id}>
+            <Card className='ProjectCard'>
+                <Card.Img variant="top" src={myItem.img_one} />
+                <Card.Body>
+                    <Card.Title className='serviceName'>{myItem.project_name}</Card.Title>
+                    <Card.Text className='serviceDescription'>
+                        {myItem.project_description}
+                    </Card.Text>
+                    <Button variant="primary"><Link className='link-style' to='/projectDetails'>View More</Link> </Button>
+                </Card.Body>
+            </Card>
+        </Col>
+    ));
+
     return (
         <Fragment>
             <Container className='text-center mt-5 p-5'>
                 <h1 className='serviceMainTitle'>RECENT PROJECTS</h1>
                 <div className='bottom'></div>
                 <Row>
-                    <Col lg={4} md={6} sm={12}>
-                        <Card className='ProjectCard'>
-                            <Card.Img variant="top" src={image3} />
-                            <Card.Body>
-                                <Card.Title className='serviceName'>Project Name one</Card.Title>
-                                <Card.Text className='serviceDescription'>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary"> <Link className='link-style' to='/projectDetails'>View More</Link> </Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col lg={4} md={6} sm={12}>
-                        <Card className='ProjectCard'>
-                            <Card.Img variant="top" src={image3} />
-                            <Card.Body>
-                                <Card.Title className='serviceName'>Project Name Two</Card.Title>
-                                <Card.Text className='serviceDescription'>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary"> <Link className='link-style' to='/projectDetails'>View More</Link> </Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col lg={4} md={6} sm={12}>
-                        <Card className='ProjectCard'>
-                            <Card.Img variant="top" src={image3} />
-                            <Card.Body>
-                                <Card.Title className='serviceName'>Project Name Three</Card.Title>
-                                <Card.Text className='serviceDescription'>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary"> <Link className='link-style' to='/projectDetails'>View More</Link> </Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                    {MyView}
                 </Row>
             </Container>
         </Fragment>
