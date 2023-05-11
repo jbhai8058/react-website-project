1651e16a2cbde53a557f7978d28497dee81f668e
Muhammad Junaid Rehman <jbhai8058@gmail.com>
2023-05-11 14:54:25 +0500

    App Url

---
+++ src/Rest Api/AppUrl.js
@@ -0,0 +1,27 @@
+class AppUrl {
+
+    static BaseURL = 'http://localhost/react_website_project_api/public/api';
+
+    static HomeTopTitle     = this.BaseURL + '/homepage/title';
+    static HomeTechDesc     = this.BaseURL + '/techhome'      ;
+    static TotalHomeDetails = this.BaseURL + '/totalhome'     ;
+    static HomeVideo        = this.BaseURL + '/home/video'    ;
+
+    static ProjectDetails   = this.BaseURL + '/projectdetails';
+    static ProjectAll       = this.BaseURL + '/projectall'    ;
+    static ProjectHome      = this.BaseURL + '/projecthome'   ;
+    static Services         = this.BaseURL + '/services'      ;
+
+    static Information      = this.BaseURL + '/coursehome'    ;
+    static FooterData       = this.BaseURL + '/courseall'     ;
+    static CourseDetails    = this.BaseURL + '/coursedetails' ;
+    static CourseAll        = this.BaseURL + '/information'   ;
+    static CourseHome       = this.BaseURL + '/footerdata'    ;
+
+    static ContactSend      = this.BaseURL + '/contactsend'   ;
+    static ClientReview     = this.BaseURL + '/clientreview'  ;
+    static ChartData        = this.BaseURL + '/chartdata'     ;
+
+}
+
+export default AppUrl
