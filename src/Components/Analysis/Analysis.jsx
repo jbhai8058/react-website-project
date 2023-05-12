1a0cfd0a39be91cf8fd931e241a739e7603f7073
Muhammad Junaid Rehman <jbhai8058@gmail.com>
2023-05-12 18:29:21 +0500

    make Analysis  page dynamically

--- src/Components/Analysis/Analysis.jsx
+++ src/Components/Analysis/Analysis.jsx
@@ -3,19 +3,29 @@ import { Col, Container, Row } from 'react-bootstrap'
 import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis } from 'recharts';
 import AppUrl from '../../Rest Api/AppUrl';
 import RestClient from '../../Rest Api/RestClient';
+import HTMLReactParser from 'html-react-parser';
 
 const Analysis = () => {
 
 
     const [data, setData] = useState(null);
+    const [techdes, settechdes] = useState('');
 
     useEffect(() => {
         RestClient.GetRequest(AppUrl.ChartData).then(result => {
-          setData(result);
+            setData(result);
         }).catch((error) => {
             console.log(error);
         })
-      }, []);
+    }, []);
+
+    useEffect(() => {
+        RestClient.GetRequest(AppUrl.HomeTechDesc).then(result => {
+            settechdes(result[0]['tech_description']);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }, []);
 
 
     const blue = "#051b35";
@@ -36,10 +46,8 @@ const Analysis = () => {
                         </ResponsiveContainer>
                     </Col>
                     <Col lg={6} md={12} sm={12}>
-                        <p className='text-justify serviceDescription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sint ea exercitationem, doloremque error atque voluptate esse, consequatur suscipit quas ab adipisci nesciunt consectetur quae architecto cumque aspernatur vero nisi?<br /><br />
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque beatae, quos voluptates aut consequatur necessitatibus unde soluta, tempora laborum dolorum odio sint tempore reprehenderit repellat velit, eaque molestias aspernatur delectus?<br /><br />
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est commodi facere sint. Eos pariatur dolorem quibusdam, accusantium officiis nam quos in tenetur dolore amet, porro, alias fuga non minima? Soluta?<br /><br />
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere veritatis tempore nulla reprehenderit maiores, ea velit ratione similique, labore corrupti quia eos officia aperiam est temporibus optio omnis aspernatur possimus!
+                        <p className='text-justify serviceDescription'>
+                            {HTMLReactParser(techdes)}
                         </p>
                     </Col>
                 </Row>
