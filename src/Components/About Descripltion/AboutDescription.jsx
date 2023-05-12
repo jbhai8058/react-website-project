efa69e3493c3e1edfe33d1b28ad71de6e3bfa089
Muhammad Junaid Rehman <jbhai8058@gmail.com>
2023-05-12 18:39:56 +0500

    make About Description page dynamically

--- src/Components/About Descripltion/AboutDescription.jsx
+++ src/Components/About Descripltion/AboutDescription.jsx
@@ -1,68 +1,29 @@
 import React, { Fragment } from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import RestClient from '../../Rest Api/RestClient';
+import AppUrl from '../../Rest Api/AppUrl';
+import HTMLReactParser from 'html-react-parser';
 
 const AboutDescription = () => {
+
+    const [aboutdesc, setaboutdesc] = useState('');
+
+    useEffect(() => {
+        RestClient.GetRequest(AppUrl.Information).then((result) => {
+            setaboutdesc(result[0]['about']);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }, [])
+
     return (
         <Fragment>
             <Container className='mt-5'>
                 <Row>
                     <Col lg={12} md={12} sm={12}>
-                        <h1 className='serviceName'>Who I Am</h1>
-                        <hr />
-                        <p className='serviceDescription'>
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, debitis, mollitia at nobis voluptatem error alias iure earum ipsam quos consequuntur laboriosam architecto dignissimos quidem atque assumenda ab saepe unde. <br /><br />
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repudiandae harum pariatur unde aliquid optio cupiditate, dolore voluptatum id, maiores, reprehenderit quod iure eius hic cum. Labore voluptatibus vero doloremque? <br /><br />
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic nostrum quia. Repudiandae molestiae nulla vel! Placeat, aliquid, perferendis repellat vel totam consequatur magni laudantium tenetur eligendi quaerat dolores inventore. <br /><br />
-
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam esse ex excepturi alias dicta sunt ut quaerat impedit, vel corporis, illum asperiores totam nemo id. Perspiciatis sit suscipit velit repellendus?
-                        </p>
-
-                        <br /><br /><br />
-
-                        <h1 className='serviceName'>Our Mission</h1>
-                        <hr />
-                        <p className='serviceDescription'>
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, debitis, mollitia at nobis voluptatem error alias iure earum ipsam quos consequuntur laboriosam architecto dignissimos quidem atque assumenda ab saepe unde. <br /><br />
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repudiandae harum pariatur unde aliquid optio cupiditate, dolore voluptatum id, maiores, reprehenderit quod iure eius hic cum. Labore voluptatibus vero doloremque? <br /><br />
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic nostrum quia. Repudiandae molestiae nulla vel! Placeat, aliquid, perferendis repellat vel totam consequatur magni laudantium tenetur eligendi quaerat dolores inventore. <br /><br />
-
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam esse ex excepturi alias dicta sunt ut quaerat impedit, vel corporis, illum asperiores totam nemo id. Perspiciatis sit suscipit velit repellendus?
-                        </p>
-
-                        <br /><br /><br />
-
-
-                        <h1 className='serviceName'>Our Vision</h1>
-                        <hr />
-                        <p className='serviceDescription'>
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, debitis, mollitia at nobis voluptatem error alias iure earum ipsam quos consequuntur laboriosam architecto dignissimos quidem atque assumenda ab saepe unde. <br /><br />
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repudiandae harum pariatur unde aliquid optio cupiditate, dolore voluptatum id, maiores, reprehenderit quod iure eius hic cum. Labore voluptatibus vero doloremque? <br /><br />
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic nostrum quia. Repudiandae molestiae nulla vel! Placeat, aliquid, perferendis repellat vel totam consequatur magni laudantium tenetur eligendi quaerat dolores inventore. <br /><br />
-
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam esse ex excepturi alias dicta sunt ut quaerat impedit, vel corporis, illum asperiores totam nemo id. Perspiciatis sit suscipit velit repellendus?
-                        </p>
-
-                        <br /><br /><br />
-
-
-                        <h1 className='serviceName'>Our Target</h1>
-                        <hr />
-                        <p className='serviceDescription'>
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, debitis, mollitia at nobis voluptatem error alias iure earum ipsam quos consequuntur laboriosam architecto dignissimos quidem atque assumenda ab saepe unde. <br /><br />
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui repudiandae harum pariatur unde aliquid optio cupiditate, dolore voluptatum id, maiores, reprehenderit quod iure eius hic cum. Labore voluptatibus vero doloremque? <br /><br />
-
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic nostrum quia. Repudiandae molestiae nulla vel! Placeat, aliquid, perferendis repellat vel totam consequatur magni laudantium tenetur eligendi quaerat dolores inventore. <br /><br />
-
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam esse ex excepturi alias dicta sunt ut quaerat impedit, vel corporis, illum asperiores totam nemo id. Perspiciatis sit suscipit velit repellendus?
-                        </p>
-
+                        {HTMLReactParser(aboutdesc)}
                     </Col>
                 </Row>
             </Container>
