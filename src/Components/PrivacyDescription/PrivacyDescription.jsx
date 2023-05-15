b20c6073f8f715c783b3c555d773f136966ee206
Muhammad Junaid Rehman <jbhai8058@gmail.com>
2023-05-15 18:11:23 +0500

    make Privacy Description page dynamically

--- src/Components/PrivacyDescription/PrivacyDescription.jsx
+++ src/Components/PrivacyDescription/PrivacyDescription.jsx
@@ -1,27 +1,29 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import RestClient from '../../Rest Api/RestClient';
+import AppUrl from '../../Rest Api/AppUrl';
+import HTMLReactParser from 'html-react-parser';
 
 const PrivacyDescription = () => {
+
+    const [data, setdata] = useState('');
+
+    useEffect(() => {
+        RestClient.GetRequest(AppUrl.Information).then((response) => {
+            setdata(response[0]['privacy']);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }, [])
+
+
+
     return (
         <Fragment>
             <Container className='mt-5'>
                 <Row>
-                <Col lg={12} md={12} sm={12}>
-                        <h1 className='serviceName'>Privacy And Policy</h1>
-                        <hr />
-                        <p className='serviceDescription'>
-                            <b>Comments</b> <br /><br />
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta nam nesciunt aperiam consectetur, veniam recusandae, nobis itaque nulla possimus architecto ullam at vero dolor vitae consequatur placeat aliquam, obcaecati laudantium?<br /><br />
-
-                            <b>Remarks</b> <br /><br />
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta nam nesciunt aperiam consectetur, veniam recusandae, nobis itaque nulla possimus architecto ullam at vero dolor vitae consequatur placeat aliquam, obcaecati laudantium?<br /><br />
-
-                            <b>Conditions</b> <br /><br />
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta nam nesciunt aperiam consectetur, veniam recusandae, nobis itaque nulla possimus architecto ullam at vero dolor vitae consequatur placeat aliquam, obcaecati laudantium?<br /><br />
-
-                            <b>Terms</b> <br /><br />
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta nam nesciunt aperiam consectetur, veniam recusandae, nobis itaque nulla possimus architecto ullam at vero dolor vitae consequatur placeat aliquam, obcaecati laudantium?<br /><br />
-                        </p>
+                    <Col lg={12} md={12} sm={12}>
+                        {HTMLReactParser(data)}
                     </Col>
                 </Row>
             </Container>
