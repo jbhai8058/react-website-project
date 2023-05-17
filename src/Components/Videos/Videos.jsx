17b372078b5dadfefd93368aca879f554d6fd3f3
Muhammad Junaid Rehman <jbhai8058@gmail.com>
2023-05-17 22:35:00 +0500

    make Video section dynamically

--- src/Components/Videos/Videos.jsx
+++ src/Components/Videos/Videos.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVideoSlash } from '@fortawesome/free-solid-svg-icons';
 import { Fragment } from 'react'
@@ -6,14 +6,27 @@ import { Button, Col, Container, Modal, Row } from 'react-bootstrap'
 import { useState } from 'react';
 import 'video-react/dist/video-react.css'; // import css
 import { BigPlayButton, Player } from 'video-react';
+import RestClient from '../../Rest Api/RestClient';
+import AppUrl from '../../Rest Api/AppUrl';
+import HTMLReactParser from 'html-react-parser';
 
 const Videos = () => {
 
 
+    const [videodescription , setvideodescription ] = useState('');
+    const [videourl , setvideourl ] = useState('');
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+
+    useEffect(()=>{
+        RestClient.GetRequest(AppUrl.HomeVideo).then((response)=>{
+            setvideodescription(response[0]['video_description']);
+            setvideourl(response[0]['video_url']);
+        })
+    },[])
 
 
     return (
@@ -23,12 +36,7 @@ const Videos = () => {
                 <div className='bottom'></div>
                 <Row>
                     <Col lg={6} md={6} sm={12}>
-                        <p className='text-justify serviceDescription'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem sunt autem doloribus consectetur omnis fugit, neque temporibus tempore quaerat quos earum accusamus, itaque esse numquam saepe repudiandae voluptatum alias non. <br /><br />
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum amet facilis expedita distinctio eaque assumenda libero soluta qui provident. Optio debitis tenetur rerum inventore adipisci facilis, totam dicta similique eaque! <br /><br />
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam eligendi ullam magnam? Cupiditate corrupti cumque enim maiores nobis itaque perspiciatis dicta magnam, animi sit, id saepe iusto laudantium corporis architecto?<br /><br />
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum fugiat provident ducimus, sed laborum debitis sit est aut quod, et corporis vel blanditiis. Sapiente nulla et possimus, cumque impedit molestias.
-                        </p>
+                        {HTMLReactParser(videodescription)}
                     </Col>
 
                     <Col lg={6} md={6} sm={12} className='VideoCard'>
@@ -41,7 +49,7 @@ const Videos = () => {
 
             <Modal size='lg' show={show} onHide={handleClose}>
                 <Modal.Body>
-                    <Player src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
+                    <Player src={videourl}>
                         <BigPlayButton position="center" />
                     </Player>
                 </Modal.Body>
