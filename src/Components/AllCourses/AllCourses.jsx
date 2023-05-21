813f2a94788d8c5f41fa892e08291c9e8c9a24a9
Muhammad Junaid Rehman <jbhai8058@gmail.com>
2023-05-21 22:20:35 +0500

    setup loading component

--- src/Components/AllCourses/AllCourses.jsx
+++ src/Components/AllCourses/AllCourses.jsx
@@ -3,49 +3,59 @@ import RestClient from '../../Rest Api/RestClient';
 import AppUrl from '../../Rest Api/AppUrl';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Loading from '../Loading/Loading';
 
 const AllCourses = () => {
 
     const [Data, setdata] = useState([]);
+    const [loading, setloading] = useState(true)
 
     useEffect(() => {
         RestClient.GetRequest(AppUrl.CourseAll).then((result) => {
             setdata(result);
+            setloading(false)
         }).catch((error) => {
             console.log(error);
         })
     }, [])
 
-    const MyView = Data ? (Data.map(myItem => (
-        <Col lg={6} md={12} sm={12} key={myItem.id}>
-            <Row>
-                <Col lg={6} md={6} sm={12} className="p-2" >
-                    <img className="courseImg" src={myItem.small_img} alt={myItem.short_title} />
+    if (loading === true) {
+        return <Loading />
+    } else {
 
-                </Col>
-
-                <Col lg={6} md={6} sm={12}>
-                    <h5 className="text-justify serviceName">{myItem.short_title}  </h5>
-                    <p className="text-justify serviceDescription">{myItem.short_description}</p>
-                    <Link className="courseViewMore float-left" to={"/coursedetails/" + myItem.id + "/" + myItem.short_title} >View Details</Link>
-                </Col>
-
-            </Row>
-        </Col>
-    ))
-    ) : null;
-
-    return (
-        <Fragment>
-            <Container className='text-center'>
-                <h1 className='serviceMainTitle'>RECENT COURSES</h1>
-                <div className='bottom'></div>
+        const MyView = Data ? (Data.map(myItem => (
+            <Col lg={6} md={12} sm={12} key={myItem.id}>
                 <Row>
-                    {MyView}
+                    <Col lg={6} md={6} sm={12} className="p-2" >
+                        <img className="courseImg" src={myItem.small_img} alt={myItem.short_title} />
+
+                    </Col>
+
+                    <Col lg={6} md={6} sm={12}>
+                        <h5 className="text-justify serviceName">{myItem.short_title}  </h5>
+                        <p className="text-justify serviceDescription">{myItem.short_description}</p>
+                        <Link className="courseViewMore float-left" to={"/coursedetails/" + myItem.id + "/" + myItem.short_title} >View Details</Link>
+                    </Col>
+
                 </Row>
-            </Container>
-        </Fragment>
-    )
+            </Col>
+        ))
+        ) : null;
+
+
+        return (
+            <Fragment>
+                <Container className='text-center'>
+                    <h1 className='serviceMainTitle'>RECENT COURSES</h1>
+                    <div className='bottom'></div>
+                    <Row>
+                        {MyView}
+                    </Row>
+                </Container>
+            </Fragment>
+        )
+
+    }
 }
 
 export default AllCourses
--- src/Components/AllProjects/AllProjects.jsx
+++ src/Components/AllProjects/AllProjects.jsx
@@ -6,46 +6,54 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import RestClient from '../../Rest Api/RestClient';
 import AppUrl from '../../Rest Api/AppUrl';
+import Loading from '../Loading/Loading';
 
 const AllProjects = () => {
 
     const [Data, setdata] = useState([]);
+    const [loading, setloading] = useState(true)
 
     useEffect(() => {
         RestClient.GetRequest(AppUrl.ProjectAll).then((result) => {
             setdata(result);
+            setloading(false)
         }).catch((error) => {
             console.log(error);
         })
     }, [])
 
-    const MyView = Data ? (Data.map(myItem => (
-        <Col lg={4} md={6} sm={12} key={myItem.id}>
-            <Card className='ProjectCard'>
-                <Card.Img variant="top" src={myItem.img_one} alt={myItem.project_name} />
-                <Card.Body>
-                    <Card.Title className='serviceName'>{myItem.project_name}</Card.Title>
-                    <Card.Text className='serviceDescription'>
-                        {myItem.project_description}
-                    </Card.Text>
-                    <Button variant="primary"> <Link className='link-style' to={'/projectDetails/'+ myItem.id + '/' +myItem.project_name}>View More</Link> </Button>
-                </Card.Body>
-            </Card>
-        </Col>
-    ))
-    ) : null;
+    if (loading === true) {
+        return <Loading />
+    } else {
 
-    return (
-        <Fragment>
-            <Container className='text-center mt-5 p-5'>
-                <h1 className='serviceMainTitle'>RECENT PROJECTS</h1>
-                <div className='bottom'></div>
-                <Row>
-                   {MyView}
-                </Row>
-            </Container>
-        </Fragment>
-    )
+        const MyView = Data ? (Data.map(myItem => (
+            <Col lg={4} md={6} sm={12} key={myItem.id}>
+                <Card className='ProjectCard'>
+                    <Card.Img variant="top" src={myItem.img_one} alt={myItem.project_name} />
+                    <Card.Body>
+                        <Card.Title className='serviceName'>{myItem.project_name}</Card.Title>
+                        <Card.Text className='serviceDescription'>
+                            {myItem.project_description}
+                        </Card.Text>
+                        <Button variant="primary"> <Link className='link-style' to={'/projectDetails/' + myItem.id + '/' + myItem.project_name}>View More</Link> </Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+        ))
+        ) : null;
+
+        return (
+            <Fragment>
+                <Container className='text-center mt-5 p-5'>
+                    <h1 className='serviceMainTitle'>RECENT PROJECTS</h1>
+                    <div className='bottom'></div>
+                    <Row>
+                        {MyView}
+                    </Row>
+                </Container>
+            </Fragment>
+        )
+    }
 }
 
 export default AllProjects
